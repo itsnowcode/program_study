947609d7d54bcffc33d29a0c106da80dcd59b253
its <itsnowdev@gmail.com>
2025-07-22 23:01:46 +0900

    Add component-04 issue

---
+++ angular/component/component-04/src/app/message-card/message-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-card',
+  imports: [],
+  templateUrl: './message-card.component.html',
+  styleUrl: './message-card.component.scss'
+})
+export class MessageCardComponent {
+
+}
